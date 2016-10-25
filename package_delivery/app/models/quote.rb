516d7e5c20e3950e5861be87d774167a714e6384
Tim Moffett <tim.a.moffett@hotmail.com>
2016-10-25 02:29:36 +0000

    pull git

--- package_delivery/app/models/quote.rb
+++ package_delivery/app/models/quote.rb
@@ -1,19 +1,18 @@
 class Quote < ActiveRecord::Base
-    validates :quantity, presence: true,
-                        length: { minimum: 1}
+    validates :quantity, presence: true
+                        
     
     validates :height, presence: true,
-                        length: { minimum: 1}
-    validates :heightunits, :numericality => {:only_double => true},
-    presence: true
+                        :inclusion => 0..40
+   
     
     validates :width, presence: true,
-                        length: { minimum: 1}
+                        :inclusion => 0..40
     
     validates :length, presence: true,
-                        length: { minimum: 1}
+                        :inclusion => 0..40
     
     validates :weight, presence: true,
-                        length: { minimum: 1}
+                        :inclusion => 0..40
     
 end
